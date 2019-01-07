06f1644ad9a29f02bf7ff562c8f93297203331df
Podvalnyi Mikhail <qwertypomy@gmail.com>
2019-01-07 19:40:42 +0200

    Implemented deleteProfile mutation

--- src/resolvers/Mutation/profile.js
+++ src/resolvers/Mutation/profile.js
@@ -4,6 +4,11 @@ const profile = {
   updateProfile(parent, { name }, ctx, info) {
     const id = getUserId(ctx);
     return ctx.db.mutation.updateUser({ data: { name }, where: { id } }, info);
+  },
+
+  deleteProfile(parent, { name }, ctx, info) {
+    const id = getUserId(ctx);
+    return ctx.db.mutation.deleteUser({ where: { id } }, info);
   }
 };
 
